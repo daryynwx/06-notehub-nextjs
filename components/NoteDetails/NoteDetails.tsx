b2ea2dd5db4d70682e36035c5b0cbb77384ea414
Darina Boinik <darinaboinik@gmail.com>
2025-07-07 12:56:12 +0300

    add all

--- components/NoteDetails/NoteDetails.tsx
+++ components/NoteDetails/NoteDetails.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useParams, useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
 import Link from 'next/link';
