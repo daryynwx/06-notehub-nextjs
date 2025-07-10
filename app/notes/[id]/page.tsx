5641aede939fa14701c9ef179e233cda1f43b0a3
Darina Boinik <darinaboinik@gmail.com>
2025-07-10 09:44:53 +0300

    add all changes

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,9 +2,11 @@ import NoteDetailsClient from '@/components/NoteDetails/NoteDetails';
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
 
-type NoteDetailsPageProps = {
-  params: { id: string };
-};
+interface NoteDetailsPageProps {
+  params: {
+    id: string;
+  };
+}
 
 export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
   const noteId = Number(params.id);
